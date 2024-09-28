d72c6d6f166e863ee281db4404883e5d7adeb9c4
MuhammadHakim33 <mhmd.hakim53@gmail.com>
2024-09-28 20:06:20 +0700

    routing & book-home controller

---
+++ controllers/BookController.php
@@ -0,0 +1,91 @@
+<?php
+
+class BookController {
+
+    public function index() {
+        echo 'Book';
+    }
+
+    // Method untuk menambahkan buku
+    public function store() {
+        // Mengecek apakah field terisi
+        if (!$this->isFilled(['judul', 'penulis'])) {
+            echo "ISI SEMUA FIELD!";
+            die;
+        }
+
+        // Menangkap semua input lalu sanitasi
+        $judul = htmlspecialchars($_POST['judul']);
+        $penulis = htmlspecialchars($_POST['penulis']);
+
+        $data = array(
+            'judul' => $judul,
+            'penulis' => $penulis,
+        );
+
+        var_dump($data);
+    }
+
+    // Method untuk mengubah buku
+    public function update() {
+        // Menangkap semua input lalu sanitasi
+        $id = htmlspecialchars($_POST['id']);
+        $judul = htmlspecialchars($_POST['judul']);
+        $penulis = htmlspecialchars($_POST['penulis']);
+
+        $data = array(
+            'id' => $id,
+            'judul' => $judul,
+            'penulis' => $penulis,
+        );
+
+        var_dump($data);
+    }
+
+    // Method untuk menghapus buku
+    public function delete($id) {
+        var_dump($id);
+    }
+
+    // Method untuk meminjam buku
+    public function borrow() {
+        // Mengecek apakah field terisi
+        if (!$this->isFilled(['id_buku', 'nomor_anggota', 'tgl_pinjam', 'tgl_kembali'])) {
+            echo "ISI SEMUA FIELD!";
+            die;
+        }
+
+        // Menangkap semua input lalu sanitasi
+        $id_buku = htmlspecialchars($_POST['id_buku']);
+        $nomor_anggota = htmlspecialchars($_POST['nomor_anggota']);
+        $tgl_pinjam = htmlspecialchars($_POST['tgl_pinjam']);
+        $tgl_kembali = htmlspecialchars($_POST['tgl_kembali']);
+        $kode_pinjam = 'PJM'.time().rand(100,999);
+
+        $data = array(
+            'kode_pinjam' => $kode_pinjam,
+            'id_buku' => $id_buku,
+            'nomor_anggota' => $nomor_anggota,
+            'tgl_pinjam' => $tgl_pinjam,
+            'tgl_kembali' => $tgl_kembali,
+        );
+
+        var_dump($data);
+        // header('Location: /');
+    }
+
+    // Method mengembalikan buku
+    public function return($id_peminjaman) {
+        echo $id_peminjaman;
+    }
+
+    // Method untuk mengecek apakah field terisi
+    private function isFilled($fields) {
+        foreach ($fields as $field) {
+            if (empty($_POST[$field])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once 'controllers/HomeController.php';
+require_once 'controllers/BookController.php';
+
+// Membersihkan url
+$url = ltrim($_SERVER['REQUEST_URI'], '/');
+$url = explode('/', $url);
+
+// Default
+$controller = 'home';
+$method = 'index';
+$params = [];
+
+
+if(!empty($url[0])) {
+    $controller = $url[0];
+    unset($url[0]);
+}
+if(!empty($url[1])) {
+    $method = $url[1];
+    unset($url[1]);
+}
+if(!empty($url)) {
+    $params = $url;
+}
+
+
+// Routing
+switch($controller) {
+    case 'home':
+        $home = new HomeController();
+        call_user_func_array([$home, $method], $params);
+        break;
+    case 'book':
+        $book = new BookController();
+        call_user_func_array([$book, $method], $params);
+        break;
+    default:
+        echo '404';
+        break;
+}
+
+
+
+// if(!method_exists($book, $url[1])) {
+//     call_user_func_array([$book, 'index'], $params);
+//     break;
+// }
+// var_dump($url);
+// var_dump($controller);
+// var_dump($method);
+// die;
+
+// var_dump($params);
+
+// echo $controller;
+// echo '<br>';
+// echo $method;
+// echo '<br>';
+// var_dump($params);
+
+
+// case 'home/book':
+//     $home->book();
+//     break;
+// case 'home/return':
+//     $id_peminjaman = $url[2];
+//     $home->bringback($id_peminjaman);
+//     break;
